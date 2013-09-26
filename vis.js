a2b9c0e29b268ea20aaaf1f0f02b752533fe845e
Tim Ruffles <timruffles@googlemail.com>
2013-09-26 09:38:51 +0100

    extended and commented code

--- vis.js
+++ vis.js
@@ -1,120 +1,151 @@
-d3.jsonp = function (url, callback) {
-  function rand() {
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-      c = '', i = -1;
-    while (++i < 15) c += chars.charAt(Math.floor(Math.random() * 52));
-    return c;
-  }
+var redditSvg;
+var previousData;
 
-  function create(url) {
-    var e = url.match(/callback=d3.jsonp.(\w+)/),
-      c = e ? e[1] : rand();
-    d3.jsonp[c] = function(data) {
-      callback(data);
-      delete d3.jsonp[c];
-      script.remove();
-    };
-    return 'd3.jsonp.' + c;
-  }
+var RADIUS = 100;
+var MIN_RADIUS = 10;
+var PADDING = 50;
+var POLL_SPEED = 2000;
 
-  var cb = create(url),
-    script = d3.select('head')
-    .append('script')
-    .attr('type', 'text/javascript')
-    .attr('src', url.replace(/(\{|%7B)callback(\}|%7D)/, cb));
-};
+function redditVis() {
+  // setup a poll requesting data, and make an immediate request
+  setInterval(requestData,POLL_SPEED);
+  requestData();
 
-function threeCircles() {
-
-	// create a root svg
-	var svg = d3.select("body")
-				.append("svg")
-				.attr("width",document.body.clientWidth)
-				.attr("height",document.body.clientHeight);
-	// bind some data
-	var circles = svg.selectAll("circle")
-					 .data([
-					 	{size: 5},
-					 	{size: 10},
-					 	{size: 15}
-					 ]);
-	// draw some circles
-	circles.enter()
-		   .append("circle")
-		   .attr("r",function(d){
-		   	 return d.size;
-		   })
-		   .attr("cx",function() {
-		   	 return 50 + Math.random() * (document.body.clientWidth - 50)
-		   })
-		   .attr("cy",function() {
-		   	 return 50 + Math.random() * (document.body.clientHeight - 50)
-		   })
+  // initial setup only needs to happen once 
+  // - we don't want to append multiple root SVG elements
+  redditSvg = d3.select("body")
+        .append("svg")
+        .attr("width",document.body.clientWidth - 50)
+        .attr("height",(RADIUS + PADDING) * 12);
 }
 
-var redditSvg;
-function redditVis() {
-	setInterval(function() {
-		d3.jsonp("http://www.reddit.com/.json?jsonp=runVis&cb=" + Math.random());
-	},2000);
-	d3.jsonp("http://www.reddit.com/.json?jsonp=runVis&cb=" + Math.random());
-	redditSvg = d3.select("body")
-				.append("svg")
-				.attr("width",document.body.clientWidth - 50)
-				.attr("height",document.body.clientHeight - 50);
+function requestData() {
+  // our jsonp url, with a cache-busting query parameter
+  d3.jsonp("http://www.reddit.com/.json?jsonp=runVis&noCache=" + Math.random());
 }
 
 function runVis(data) {
-	var formatted = formatRedditData(data);
 
-	var stories = redditSvg
-	   .selectAll(".story")
-	   .data(formatted,function(d) {
-	   	 return d.id;
-	   });
+  // d3 never does anything automagical to your data
+  // so we'll need to get data into the right format, with the
+  // previous values attached
+  var formatted = formatRedditData(data,previousData);
 
-	stories.forEach(function(d) {  d.score = Math.E * d.score })
+  // setup our radius scale - this maps from the min & max of scores
+  // to min and max radius we've set above
+  var radiusScale = d3.scale.linear()
+    .domain(d3.extent(formatted,function(d) {
+      return d.score;
+    }))
+    .range([MIN_RADIUS,RADIUS]);
 
-	var radiusScale = d3.scale.linear()
-		.domain(d3.extent(formatted,function(d) {
-			return d.score;
-		}))
-		.range([10,100]);
+  // select our stories, pulling in previous ones to update
+  // by selecting on the stories' class name
+  var stories = redditSvg
+     .selectAll(".story")
+     // the return value of data() is the update context - so the 'stories' var is
+     // how we refence the update context from now on
+     .data(formatted,function(d) {
+       // use a key function to ensure elements are always bound to the same 
+       // story (especially important when data enters/exits)
+       return d.id;
+     });
 
-	var row = Math.floor(document.body.clientWidth / 200);
+  // calculate how many circles we'll get on a row
+  var row = Math.floor((document.body.clientWidth - PADDING) / (RADIUS * 2));
 
-	// ENTER context
-	stories.enter()
-		   .append("g")
-		   .classed("story",true)
-		   .append("circle")
-		   .attr("cx",function(d,i) {
-		   		return (i % row) * 200;
-		   })
-		   .attr("cy",function(d,i) {
-		   		return Math.floor(i / row) * 200;
-		   })
-		   .attr("r",0)
-		   ;
+  // ENTER context
+  // here we create a group element to hold the past and previous score circles
+  var storiesEntering = stories.enter()
+       .append("g")
+       .classed("story",true)
 
-	// UPDATE + ENTER context
-    stories
-    	   .select("circle")
-    	   .transition(500)
-    	   .attr("r",function(d) {
-		   		return radiusScale(d.score);
-		   });
+  // to add multiple children in d3, capture the context of the parent in a varible
+  // and reuse it to add as many children as is required
+  storiesEntering
+       .append("circle")
+       .classed("past",true);
 
-    // EXIT content
-	stories.exit()
-		   .transition(2000)
-		   .attr("r",0);	
+  storiesEntering
+       .append("circle")
+       .classed("present",true);
+
+  // UPDATE + ENTER context
+  // elements added via enter() will then be available on the update context, so
+  // we can set attributes once, for entering and updating elements, here
+  stories
+     .attr("transform",function(d,i) {
+        var x = 100 + (i % row) * (RADIUS * 2);
+        var y = 100 + Math.floor(i / row) * (RADIUS * 2);
+        return "translate(" + x + "," + y + ")";
+     });
+
+  stories
+     // we select inside our top-level story groups to get a specific child
+     .select(".present")
+     // when we hover over a story, show the title of the story as an overlay
+     .on("mouseover",showTitle)
+     // classed takes either a boolean, or a function that returns a boolean,
+     // which decides whether d3 should ensure the class is or isn't present
+     .classed("up",function(d) {
+       return d.diff > 0
+     })
+     .classed("down",function(d) {
+       return d.diff < 0
+     })
+     // setting a transition will make all future attribute changes
+     // happen over a period - the duration is set in milliseconds
+     .transition(500)
+     .attr("r",function(d) {
+        return radiusScale(d.score);
+     });
+
+  stories
+     .select(".past")
+     .transition(500)
+     .attr("r",function(d) {
+        if(!d.previous || d.diff === 0) return 0;
+        return radiusScale(d.previous.score);
+     });
+
+  // EXIT content
+  stories.exit()
+       .select(".present, .past")
+       .transition(500)
+       .attr("r",0)
+       .remove();
+}
+
+function showTitle(d) {
+  // this is a callback on the event listener of a d3 selection, so it'll
+  // take the arguments you'd expect: datum and index
+  d3.select("#title")
+    .classed("on",true)
+    .text(d.title);
 }
 
 function formatRedditData(data) {
-	return data.data.children.map(function(story) {
-		return story.data;
-	});
+  // dig through reddit's data structure to get a flat list of stories
+  var formatted = data.data.children.map(function(story) {
+    return story.data;
+  });
+  // make a map of storyId -> previousData
+  var previousDataById = (previousData || []).reduce(function(all,d) {
+    all[d.id] = d;
+    return all;
+  },{});
+  // for each present story, see if it has a previous value,
+  // attach it and calculate the diff
+  formatted.forEach(function(d) {
+    d.previous = previousDataById[d.id];
+    d.diff = 0;
+    if(d.previous) {
+      d.diff = d.score - d.previous.score;
+    }
+  });
+  // our new data will be the previousData next time
+  previousData = formatted;
+  return formatted;
 }
 
 redditVis();
